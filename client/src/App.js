94cd8db3450725c649ff90a5891fa481134a19bd
Bhupendra sambare <bhupendrasam1404@gmail.com>
2023-05-09 00:20:32 +0530

    logical application completed

--- client/src/App.js
+++ client/src/App.js
@@ -30,6 +30,7 @@ function App() {
                 return;
             }else{
                 setConnectionNetwork(true);
+                getTaskList();
             }
 
             const account = await ethereum.request({method:"eth_requestAccounts"})
@@ -51,20 +52,11 @@ function App() {
                     TaskAbi.abi,
                     signer
                 )
-                console.log(TaskContract)
-                // await TaskContract.getMyTasks.map(data=>{
-                //     console.log(data)
-                // })
                 if(TaskContract != null && TaskContract != undefined){
                     let tempTasks = await TaskContract.getMyTasks();
                     setTasks(tempTasks)
                 }
-                // setTasks([])
-                // tempTasks.map(data=>{
-                //     setTasks([...tasks,data]);
-                // })
             }
-
         }catch(expection){
             console.log(expection)
         }
@@ -73,7 +65,7 @@ function App() {
     const saveData = () =>{
         console.log("start")
         let temp = {
-            "task":input,
+            "taskText":input,
             "isDeleted":false
         }
         try{
@@ -87,9 +79,8 @@ function App() {
                     signer
                 )
 
-                TaskContract.addTask(temp.task,temp.isDeleted)
+                TaskContract.addTask(temp.taskText,temp.isDeleted)
                 .then(response=>{
-                    console.log(response)
                     setTasks([...tasks,temp]);
                     getTaskList();
                 })
@@ -103,8 +94,37 @@ function App() {
         }
     }
 
-    const deleteTask = () =>{
+    const deleteTask = (id) =>{
         
+        console.log("delete task")
+        let temp = {
+            "id":id,
+            "isDeleted":true
+        }
+        try{
+            const {ethereum} = window
+            if(ethereum){
+                const provider = new ethers.providers.Web3Provider(ethereum);
+                const signer = provider.getSigner();
+                const TaskContract = new ethers.Contract(
+                    TaskContractAddress,
+                    TaskAbi.abi,
+                    signer
+                )
+
+                TaskContract.deleteTask(temp.id,temp.isDeleted)
+                .then(response=>{
+                    setTasks([...tasks,temp]);
+                    getTaskList();
+                })
+                .catch(error=>{
+                    console.log(error);
+                });
+            }
+
+        }catch(expection){
+
+        }
     }
 
     useEffect(()=>{
@@ -116,21 +136,24 @@ function App() {
         <>
         <Navbar className='shadow py-2'>
             <Container>
-                <Navbar.Brand href="#home">Your Todo List</Navbar.Brand>
+                <Navbar.Brand href="#home">Do's</Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
                 <Navbar.Text>
-                    {(currentAccount)?currentAccount:<><button onClick={connectWallet} className='btn btn-dark'>Connect wallet</button></>}
+                    
                 </Navbar.Text>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
         <div className="App" onClick={connectWallet}>
             <div className="container shadow p-5 mt-5">
-                <h3>TODO Tasks</h3>
+                <div className=" justify-content-between d-flex flex-wrap">
+                    <h3>TODO Tasks</h3>
+                    <h5>{(currentAccount)?<div className='text-break'>{currentAccount}</div>:<><button onClick={connectWallet} className='btn btn-dark'>Connect wallet</button></>}</h5>
+                </div>
                 <div className='my-4'>
                     <label htmlFor="task" className='my-2'>Enter task</label>
-                    <textarea type="text" id='task' placeholder='Enter task' className="form-control" rows={5} onChange={(e)=>setInput(e.target.value)}>
+                    <textarea type="text" id='task' placeholder='Enter task' className="form-control" rows={2} onChange={(e)=>setInput(e.target.value)}>
                     
                     </textarea><br/>
                     <div className="w-100 d-flex">
@@ -139,16 +162,24 @@ function App() {
                 </div>
 
                 <h5 className='my-2'>Your Tasks</h5>
-                <ul>
-                    {
-                        tasks.map(aa=>
-                            {
-                                console.log(aa);
-                                return <><li>{aa.taskText}</li></> 
-                        })
+                <div className="d-flex justify-content-center">
+                    <ul className='task-box'>
+                        {
+                            tasks.map(aa=>
+                                {
+                                    return <><li className="justify-content-center shadow mb-2 d-flex px-2 py-4">
+                                        <div className=''>
+                                            {aa.taskText}
+                                        </div>
+                                        <div onClick={()=>deleteTask(aa.id?._hex)} role="button" className="text-danger ms-auto text-weight-bold">
+                                            Delete
+                                        </div>
+                                        </li></> 
+                            })
 
-                    }
-                </ul>
+                        }
+                    </ul>
+                </div>
             </div>
         </div>
         </>
